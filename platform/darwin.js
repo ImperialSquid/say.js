49c38280c2b9052a4d5d5b8f2f73009fb4e93d50
Ben Jaeger <benjaeger99@gmail.com>
2023-04-24 07:08:43 +0100

    feat: add getInstalledVoices for macos

--- platform/darwin.js
+++ platform/darwin.js
@@ -1,4 +1,6 @@
 const SayPlatformBase = require('./base.js')
+const childProcess = require('child_process')
+const once = require('one-time')
 
 const BASE_SPEED = 175
 const COMMAND = 'say'
@@ -55,8 +57,42 @@ class SayPlatformDarwin extends SayPlatformBase {
     this.child.kill()
   }
 
-  getVoices () {
-    throw new Error(`say.export(): does not support platform ${this.platform}`)
+  getVoices (callback) {
+    if (typeof callback !== 'function') {
+      callback = () => {}
+    }
+    callback = once(callback)
+
+    let voices = []
+    let command = 'say'
+    let args = ['-v', '?']
+
+    this.child = childProcess.spawn(command, args)
+
+    this.child.stdin.setEncoding('ascii')
+    this.child.stderr.setEncoding('ascii')
+
+    this.child.stderr.once('data', (data) => {
+      // we can't stop execution from this function
+      callback(new Error(data))
+    })
+    this.child.stdout.on('data', function (data) {
+      voices += data
+    })
+
+    this.child.addListener('exit', (code, signal) => {
+      if (code === null || signal !== null) {
+        return callback(new Error(`say.getInstalledVoices(): could not get installed voices, had an error [code: ${code}] [signal: ${signal}]`))
+      }
+      console.log('voices', voices)
+      if (voices.length > 0) {
+        voices = voices.split('\r\n')
+        voices = (voices[voices.length - 1] === '') ? voices.slice(0, voices.length - 1) : voices
+      }
+      this.child = null
+
+      callback(null, voices)
+    })
   }
 }
 
