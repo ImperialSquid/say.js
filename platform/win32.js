dce816beecd8fb3c775346c43583974452d516f6
Ben Jaeger <benjaeger99@gmail.com>
2023-04-24 05:48:36 +0100

    refactor: move getVoices code to win32.js
    Getting voices for in festival (linux) is too involved in current setup, refactor to allow for implementation without having to work around

--- platform/win32.js
+++ platform/win32.js
@@ -1,6 +1,7 @@
 const childProcess = require('child_process')
 
 const SayPlatformBase = require('./base.js')
+const once = require('one-time')
 
 const BASE_SPEED = 0 // Unsupported
 const COMMAND = 'powershell'
@@ -77,11 +78,45 @@ class SayPlatformWin32 extends SayPlatformBase {
     return Math.max(-10, Math.min(Math.round((9.0686 * Math.log(speed)) - 0.1806), 10))
   }
 
-  getVoices () {
+  getVoices (callback) {
+
+    if (typeof callback !== 'function') {
+      callback = () => {}
+    }
+    callback = once(callback)
+
     let args = []
     let psCommand = 'Add-Type -AssemblyName System.speech;$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer;$speak.GetInstalledVoices() | % {$_.VoiceInfo.Name}'
     args.push(psCommand)
-    return { command: COMMAND, args }
+    let command = COMMAND
+    var voices = []
+    this.child = childProcess.spawn(command, args)
+
+    this.child.stdin.setEncoding('ascii')
+    this.child.stderr.setEncoding('ascii')
+
+    this.child.stderr.once('data', (data) => {
+      // we can't stop execution from this function
+      callback(new Error(data))
+    })
+    this.child.stdout.on('data', function (data) {
+      voices += data
+    })
+
+    this.child.addListener('exit', (code, signal) => {
+      if (code === null || signal !== null) {
+        return callback(new Error(`say.getInstalledVoices(): could not get installed voices, had an error [code: ${code}] [signal: ${signal}]`))
+      }
+      if (voices.length > 0) {
+        voices = voices.split('\r\n')
+        voices = (voices[voices.length - 1] === '') ? voices.slice(0, voices.length - 1) : voices
+      }
+      this.child = null
+
+      callback(null, voices)
+    })
+
+    this.child.stdin.end()
   }
 }
 
